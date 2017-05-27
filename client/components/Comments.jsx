ae0411a1617082bc1271b0575230f6b95571eee4
Shawn Stern <stern.shawn@gmail.com>
2017-05-26 21:34:49 -0700

    Updating single to render the given post based on route params, creating template for comments section

---
+++ client/components/Comments.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Comments = (props) => (
+  <div className="comment">
+    I'm a comment
+  </div>
+);
+
+export default Comments;
--- client/components/Single.jsx
+++ client/components/Single.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Single = () => (
-  <div className="single-photo">
-    I'm a single instance of a photo
-  </div>
-);
+import Photo from './Photo';
+import Comments from './Comments';
+
+const Single = (props) => {
+  const idx = props.posts.findIndex((post) => post.code === props.params.postId);
+  const post = props.posts[idx];
+  return (
+    <div className="single-photo">
+      <Photo idx={idx} post={post} {...props} />
+      <Comments />
+    </div>
+  );
+};
 
 export default Single;
