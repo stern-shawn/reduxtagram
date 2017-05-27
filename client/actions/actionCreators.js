f65a4bbc15ef09fb54783a40dd6bf4b4e7238bee
Shawn Stern <stern.shawn@gmail.com>
2017-05-26 20:11:33 -0700

    Adding inital redux actions, reducers, and store

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,20 @@
+// Action to increment/decrement like count of a photo
+export const increment = (index) => ({
+  type: 'INCREMENT_LIKES',
+  index,
+});
+
+// Action to add a comment to a photo
+export const addComment = (postId, author, comment) => ({
+  type: 'ADD_COMMENT',
+  postId,
+  author,
+  comment,
+});
+
+// Action to remove a comment from a photo
+export const removeComment = (postId, index) => ({
+  type: 'REMOVE_COMMENT',
+  postId,
+  index,
+});
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import {
-  render
-} from 'react-dom';
+import { render } from 'react-dom';
 import {
   Router,
   Route,
   IndexRoute,
   browserHistory
 } from 'react-router';
-import css from './styles/style.styl';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
+import css from './styles/style.styl';
+
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 );
 
-render(router , document.getElementById('root'));
+render(router, document.getElementById('root'));
