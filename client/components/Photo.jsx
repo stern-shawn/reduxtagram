5257509c30c0845b2ba5cefacb558faffa2d2b75
Shawn Stern <stern.shawn@gmail.com>
2017-05-28 19:16:56 -0700

    Fixing relative urls in Photo component

--- client/components/Photo.jsx
+++ client/components/Photo.jsx
@@ -6,7 +6,7 @@ import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 const Photo = ({ post, comments, idx, increment }) => (
   <figure className="grid-figure">
     <div className="grid-photo-wrap">
-      <Link to={`view/${post.code}`}>
+      <Link to={`/view/${post.code}`}>
         <img src={post.display_src} alt={post.caption} className="grid-photo"/>
       </Link>
       <CSSTransitionGroup
@@ -21,7 +21,7 @@ const Photo = ({ post, comments, idx, increment }) => (
       <p>{post.caption}</p>
       <div className="control-buttons">
         <button className="likes" onClick={() => increment(idx)}>&hearts; {post.likes}</button>
-        <Link to={`view/${post.code}`} className="button">
+        <Link to={`/view/${post.code}`} className="button">
           <span className="comment-count">
             <span className="speech-bubble"></span>
             {comments[post.code] ? comments[post.code].length : 0 }
