136c4b09a18b2c227c40592b07c2b4c3221d487d
Shawn Stern <stern.shawn@gmail.com>
2017-05-28 19:12:54 -0700

    Adding basic support for Raven/Sentry error tracking

--- client/data/config.js
+++ client/data/config.js
@@ -1,7 +1,7 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
+const sentry_key = '027e01eb800e4686b2f91aee3da14898';
+const sentry_app = '173175';
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
 export function logException(ex, context) {
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -8,12 +8,16 @@ import {
 } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
+import Raven from 'raven-js';
+import { sentry_url } from './data/config';
 
 import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 import css from './styles/style.styl';
+
+Raven.config(sentry_url).install();
 
 const router = (
   <Provider store={store}>
